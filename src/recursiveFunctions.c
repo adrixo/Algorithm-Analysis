1b58f0d0cfb0e8d1025e8ecb6fdaa2f93f2ee58c
adrixo <adrianvalrom@gmail.com>
2018-09-20 08:48:15 +0200

    added 2 functions

--- src/recursiveFunctions.c
+++ src/recursiveFunctions.c
@@ -56,3 +56,34 @@ int iterativePow(int n, int p)
   }
   return result;
 }
+
+int mergeFindMax(int *array, int bot, int top)
+{
+  int aux1, aux2;
+
+  if(bot == top)
+    return (array[bot]);
+
+  aux1 = mergeFindMax(array, bot, (bot+top)/2 );
+  aux2 = mergeFindMax(array, ((bot+top)/2)+1, top );
+
+  if (aux1 > aux2)
+    return aux1;
+  return aux2;
+
+}
+
+int algoritm(int *array, int bot, int top, int nElements)
+{
+  int k = (bot+top)/2;
+
+  if( (k==1 || array[k-1]<=array[k]) &&
+      (k==nElements || array[k+1] <= array[k]) )
+      return k;
+
+  if( k>1 && array[k-1]>array[k] )
+    return algoritm(array, bot, k-1, nElements);
+
+return algoritm(array, k+1, k, nElements);
+
+}

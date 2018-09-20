2584da367dfc96f517614a6083dd03ec49675951
adrixo <adrianvalrom@gmail.com>
2018-09-20 08:51:02 +0200

    Deleted

--- SortingAlgorithm/bubbleSort.h
+++
@@ -1,7 +0,0 @@
-#ifndef __BUBBLESORT_H
-#define __BUBBLESORT_H
-
-void swap(int *x, int *y);
-int * bubbleSort(int *array, int length);
-
-#endif
--- Utils/array.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ARRAY_H
-#define __ARRAY_H
-
-int * createArrayRandomNumbers(int n, int max);
-void printArray(int * array, int length, char *name);
-
-#endif

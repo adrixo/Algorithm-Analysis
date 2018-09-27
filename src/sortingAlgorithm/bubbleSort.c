70e963538b5de560e3ecb5026e95bd66f986f036
adrixo <adrianvalrom@gmail.com>
2018-09-28 00:13:52 +0200

    bubble updated

---
+++ src/sortingAlgorithm/bubbleSort.c
@@ -0,0 +1,26 @@
+#include "bubbleSort.h"
+
+void swap(int *x, int *y)
+{
+  int aux = *x;
+  *x = *y;
+  *y = aux;
+}
+
+int bubbleSort(int *array, int length)
+{
+  int i, j;
+  int c = 0;
+
+  for(i = 0; i < length-1; i++)
+  {
+    for(j = 0; j < length-i-1; j++)
+    {
+      if( array[j] > array[j+1])
+        swap(&array[j], &array[j+1]);
+      c++;
+    }
+  }
+
+  return c;
+}

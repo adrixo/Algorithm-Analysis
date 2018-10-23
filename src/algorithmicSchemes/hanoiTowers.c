0d999bdbedfb60d074a41d73f037ae716614e9e1
adrixo <adrianvalrom@gmail.com>
2018-10-23 08:54:34 +0200

    Added algorithm schemes

---
+++ src/algorithmicSchemes/hanoiTowers.c
@@ -0,0 +1,10 @@
+
+int hanoi(int numberOfDisks, int origin, int destination, int aux)
+{
+  if( n>0 )
+  {
+    hanoi(numberOfDisks-1, origin, aux, destination);
+    printf("Moving disk %d from %d to %d\n", numberOfDisks, origin, destination);
+    hanoi(numberOfDisks-1, aux, destination, origin);
+  }
+}
